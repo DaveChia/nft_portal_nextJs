98fe9277b0dae1e0d0dfe7678064193a03d46386
Dave <dave.chia88@gmail.com>
2023-09-27 21:08:04 +0800

    Initialize project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+let Web3 = require('web3');
+
+function Index() {
+    return (
+      <div>
+        Welcome to your homepage
+      </div>
+    )
+}
+
+export default Index
