86fddca79fa9d2dbcc19610c252ebba87f4c863c
Dave <dave.chia88@gmail.com>
2023-09-27 21:14:44 +0800

    Integrated with web3js to integrate with Metamask chrome extension

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
-import React, {useState, useEffect} from 'react';
-let Web3 = require('web3');
+import React, { useState, useEffect } from "react";
+let Web3 = require("web3");
 
 function Index() {
-    return (
-      <div>
-        Welcome to your homepage
-      </div>
-    )
+  const [web3, setWeb3] = useState(null);
+  const [address, setAddress] = useState(null);
+
+  useEffect(() => {
+    window.ethereum
+      ? ethereum
+          .request({ method: "eth_requestAccounts" })
+          .then((accounts) => {
+            setAddress(accounts[0]);
+            let w3 = new Web3(ethereum);
+            setWeb3(w3);
+          })
+          .catch((err) => console.log(err))
+      : console.log("Please install MetaMask");
+  }, []);
+
+  return <div>Welcome to your homepage</div>;
 }
 
-export default Index
+export default Index;
